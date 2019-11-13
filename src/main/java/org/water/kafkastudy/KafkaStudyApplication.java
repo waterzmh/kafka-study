b9a2e2149bbaa5c30530db0ff472e0a035a817b0
water <903936820@qq.com>
2019-11-13 18:05:19 +0800

    Initial commit

---
+++ src/main/java/org/water/kafkastudy/KafkaStudyApplication.java
@@ -0,0 +1,16 @@
+package org.water.kafkastudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * water
+ */
+@SpringBootApplication
+public class KafkaStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaStudyApplication.class, args);
+    }
+
+}
